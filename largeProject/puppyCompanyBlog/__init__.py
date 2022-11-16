d38c4160daf9374d26a57b4580e932fd90203fff
Yago César <yagocdj@gmail.com>
2022-11-16 09:47:01 -0300

    Error handlers

--- largeProject/puppyCompanyBlog/__init__.py
+++ largeProject/puppyCompanyBlog/__init__.py
@@ -2,3 +2,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+from puppyCompanyBlog.core.views import core
+from puppyCompanyBlog.error_pages.handlers import error_pages
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
