134a7c2bb1cc0e235ddd8c027ddb9edb7bad4f42
Yago César <yagocdj@gmail.com>
2022-11-16 08:56:22 -0300

    Adding the structure of the large project

---
+++ largeProject/app.py
@@ -0,0 +1,6 @@
+# app.py
+from puppyCompanyBlog import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
---
+++ largeProject/puppyCompanyBlog/__init__.py
@@ -0,0 +1,4 @@
+# puppyCompanyBlocg/__init__.py
+from flask import Flask
+
+app = Flask(__name__)
