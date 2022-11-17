bf623ba8290ac308fd99ab6a98a4eedb4c36e090
Yago César <yagocdj@gmail.com>
2022-11-17 11:24:15 -0300

    Adding the blog related views and templates

--- largeProject/puppyCompanyBlog/__init__.py
+++ largeProject/puppyCompanyBlog/__init__.py
@@ -31,7 +31,9 @@ login_manager.login_view = 'users.login'
 from puppyCompanyBlog.core.views import core
 from puppyCompanyBlog.error_pages.handlers import error_pages
 from puppyCompanyBlog.users.views import users
+from puppyCompanyBlog.blogPosts.views import blog_posts
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
--- largeProject/puppyCompanyBlog/blogPosts/forms.py
+++ largeProject/puppyCompanyBlog/blogPosts/forms.py
@@ -1,4 +1,4 @@
-# blog_posts/forms.py
+# blogPosts/forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
