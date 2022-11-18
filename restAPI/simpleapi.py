5ca29f616bc566c9012a8f213022dd0a7fa2e2d0
Yago César <yagocdj@gmail.com>
2022-11-18 09:32:07 -0300

    CRUD in REST API

--- restAPI/simpleapi.py
+++ restAPI/simpleapi.py
@@ -2,15 +2,43 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-
 api = Api(app)
 
-class HelloWorld(Resource):
+puppies = []
+
+class PuppyNames(Resource):
+    
+    # All of the methods must have the same parameters
+    def get(self, name):
+        
+        for pup in puppies:
+            if pup['name'] == name:
+                return pup
+        
+        return {'name': None}, 404
+
+    def post(self, name):
+        
+        pup = {'name': name}
+        puppies.append(pup)
+        return pup
+
+    def delete(self, name):
+        
+        for index, pup in enumerate(puppies):
+            if pup['name'] == name:
+                deleted_pup = puppies.pop(index)
+                # print(deleted_pup)
+                return {'note': 'delete success'}
+
+class AllNames(Resource):
 
     def get(self):
-        return {'hello': 'world'}
+        return {'puppies': puppies}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(PuppyNames, '/puppy/<string:name>')
+api.add_resource(AllNames, '/puppies')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
